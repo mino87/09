4142a471095e13b16caf7a38270b11d4fce4e652
mino87 <113091109+mino87@users.noreply.github.com>
2022-11-01 16:38:50 +0900

    3-1

--- main.c
+++ main.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 5
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
-	int i,avg;
-	int g[5];
-	int sum=0;
-	
-	for(i=0;i<5;i++)
-	{
-	printf("학생성적을 입력하세요.:\n");
-	scanf("%d",&g[i]);
-	sum+=g[i];
-	}
-	    
-    avg = sum/5;
-    printf("성적평균 : %d\n",avg);
-    
+
+int i;
+int g[SIZE];
+int s[SIZE];
+
+for(i=0;i<SIZE;i++)
+   g[i]=rand()%100;
+for(i=0;i<SIZE;i++)
+   s[i]=g[i];
+
+for(i=0;i<SIZE;i++)
+    printf("s[%d]=%d\n",i,s[i]);
+	   
 	return 0;
 }
